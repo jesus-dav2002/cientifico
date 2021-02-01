6019c9206e406d90bdb0eed69b06769b0bcaf368
David Maldonado <jdmg_1010@hotmail.com>
2021-02-01 07:02:51 -0400

    conectar las rutas con los templates

---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,10 @@
+import router from "../routes"
+
+const resolveRoutes = () => {
+    if(router.length <= 3) {
+        let validRoute = route === '/' ? route: '/:id';
+    }
+    return `/${route}`;
+}
+
+export default resolveRoutes;
